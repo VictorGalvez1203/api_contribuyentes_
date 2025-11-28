dc183c4fda213c3c9f5be82a625c25a7a3a89c8f
VictorGalvez03 <vgalvez@dgii.gov.do>
2025-11-28 10:48:37 -0400

    Agregar archivo .gitignore y eliminar carpeta .vs del control de versiones

--- dgii_api_contribuyentes/Application/Behaviours/ValidationBehavior.cs
+++ dgii_api_contribuyentes/Application/Behaviours/ValidationBehavior.cs
@@ -17,7 +17,7 @@ namespace Application.Behaviours
             _validators = validators;
         }
 
-        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             if (_validators.Any())
             {
--- dgii_api_contribuyentes/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ dgii_api_contribuyentes/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b767b2bc01e272c14ca1c9b9d31acd961c5fa7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8e8c121671953b622e2c7d33009442f7bbfeca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
